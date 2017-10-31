8e520106a99d8e790ecfa58ebfbcd32d0a72bca0
parkito <artyom-karnov@yandex.ru>
2017-10-31 18:40:05 +0300

    Add polimorphism implementation

--- src/main/java/com/parkito/configuraton/ServletConfiguration.java
+++ src/main/java/com/parkito/configuraton/ServletConfiguration.java
@@ -1,8 +1,25 @@
 package com.parkito.configuraton;
 
+import com.parkito.implementation.FilterImplementation;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author Artem Karnov @date 10/31/2017.
  * artem.karnov@t-systems.com
  */
-public class ServletConfiguration {
+public class ServletConfiguration implements Filter {
+    private FilterImplementation filterImplementation = new FilterImplementation();
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        filterImplementation.doFilter((HttpServletRequest) request, (HttpServletResponse) response, filterChain);
+    }
 }
